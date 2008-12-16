c1f05c9e0e4ce131b25390569cf0b2eda595c8cd
tinkerpriest <tinkerpriest@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-12-16 19:28:34 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/add-logo-to-admin/trunk@80493 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/login.js
@@ -0,0 +1,14 @@
+window.onload = addLogo;
+
+function addLogo() {
+	var myBody = document.getElementsByTagName("h1")[0];
+	myBody.innerHTML = "";
+
+	var img = document.createElement("img");
+	var imgSrc = "../wp-content/plugins/add-logo/images/logo.png";
+	img.setAttribute("src",imgSrc);
+	img.setAttribute("id", "newLogo");
+	
+	myBody.appendChild(img);
+	return false;
+}
