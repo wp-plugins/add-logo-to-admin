b97b9ae776a41a56a79c1418fd801eb92f484396
tinkerpriest <tinkerpriest@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-23 22:03:46 +0000

    git-svn-id: https://plugins.svn.wordpress.org/add-logo-to-admin/tags/1.2@97013 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/admin.js
@@ -0,0 +1,28 @@
+var oldOnload = window.onload;
+
+if(typeof oldOnload == "function") {
+	window.onload = function() {
+		if(oldOnload) {
+			oldOnload();
+		}
+		addLogo();
+	}
+} else {
+	window.onload = addLogo;
+}
+			
+function addLogo() {
+	var myBody = document.getElementById("wphead").getElementsByTagName("a")[0];
+	myBody.innerHTML = "";
+
+	var img = document.createElement("img");
+	var imgSrc = newLogo.site+newLogo.logo;
+	img.setAttribute("src",imgSrc);
+	img.setAttribute("id", "newLogo");
+	
+	myBody.appendChild(img);
+	
+	myBody.style.display = "block";
+	
+	return false;
+}
